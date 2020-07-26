21c46c8cff4be715f5bbde49d5753acd197f9fed
autoB0T <debnath.sakti@gmail.com>
2020-07-26 16:52:15 +0530

    done

--- key-store.js
+++ key-store.js
@@ -1,4 +1,5 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
+const { v4: uuidv4 } = require("uuid");
 const fs = require('fs');
 const shortid = require('shortid');
 // To generate a unique API KEY, use shortid.generate()
@@ -6,6 +7,7 @@ const LINE_ENDING = require('os').EOL;
 
 
 module.exports = function (req, res) {
-
+    let uniqueID = uuidv4();
+    fs.appendFileSync(VALID_KEYS_PATH, uniqueID+LINE_ENDING);
+    return res.status(201).json({'apiKey':uniqueID});
 };
-
--- middleware.js
+++ middleware.js
@@ -1,5 +1,16 @@
 const VALID_KEYS_PATH = __dirname + '/valid-keys.txt';
-
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const LINE_ENDING = require('os').EOL;
 module.exports = function (req, res, next) {
-
+    var apiKEY = req.header('x-api-key');
+    if(apiKEY===undefined){
+        return res.status(401).json({});
+    } else {
+        let cF= fs.readFileSync(VALID_KEYS_PATH,{encoding:'utf8', flag:'r'});
+        cF = cF.split(LINE_ENDING).filter(v=>v!='');
+        if(!cF.find(function(v){ return v==apiKEY; })) return res.status(401).json({});
+    }
+    next();
 };
